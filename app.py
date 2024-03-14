56af0f7b2e233f6cd74f23fbbc369f2ced6ea991
satoshchan <bravlauk88@gmail.com>
2024-03-14 14:57:52 +0300

    Command Start and Command list

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from aiogram.filters import CommandStart
 from aiogram.types import BotCommandScopeAllPrivateChats
 
 from bot_cmds_list import command_list
+from handlers.user_private import user_private_router
 
 bot = Bot(token="7176823300:AAGzo8nLwWhOF9S4VbQQ2hXeWwQHUnT2fV8")
 
 dp = Dispatcher()
 
+dp.include_router(user_private_router)
 
-@dp.message(CommandStart())
+@user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
     await bot.send_sticker(message.from_user.id,
                            sticker="CAACAgIAAxkBAAELsPVl8tZBgN2WDzAFRNlu4ag8K05LuwAC0Q0AAstxcEvO1r8zXvNYlzQE")
@@ -18,6 +20,7 @@ async def start_cmd(message: types.Message):
         f"<em>Здравствуйте, <b>{message.from_user.full_name}</b>! Я бот магазина FF Poizon. Я могу прислать подробные фото "
         f"о каждом товаре. Для этого выберите кнопкой ниже нужную категорию "
         f"и там найдите интересующий товар. Я скину всю информацию, которая у меня есть</em>", parse_mode="HTML")
+
 
 
 async def main():
---
+++ handlers/user_private.py
@@ -0,0 +1,7 @@
+from aiogram import types, Router
+from aiogram.filters import CommandStart
+
+from app import bot
+
+user_private_router = Router()
+
