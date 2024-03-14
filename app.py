35a7a8bf79bebea9b1ec65c29dc37e826e7fd041
satoshchan <bravlauk88@gmail.com>
2024-03-14 23:26:02 +0300

    Brands Inline

--- app.py
+++ app.py
@@ -28,8 +28,54 @@ start_kb = InlineKeyboardMarkup(
             InlineKeyboardButton(text="Штаны", callback_data="trousers"),
         },
         {
+            InlineKeyboardButton(text="Брендовая/Оригинальная одежда", callback_data="brend")
+        },
+        {
             InlineKeyboardButton(text="Отзывы", url="https://t.me/ff_poizon_otzivi"),
         }
+    ],
+)
+
+brand = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Футболки", callback_data="t_shirt_brand"),
+        ],
+        {
+            InlineKeyboardButton(text="Зипки", callback_data="zip_brand"),
+        },
+        {
+            InlineKeyboardButton(text="Кофты", callback_data="sweater_brand"),
+        },
+        {
+            InlineKeyboardButton(text="Головные уборы/Очки", callback_data="cap_brand"),
+        },
+        {
+            InlineKeyboardButton(text="Кроссовки", callback_data="shoes_brand"),
+        },
+        {
+            InlineKeyboardButton(text="Штаны", callback_data="trousers_brand"),
+        },
+    ],
+)
+
+brand_shoes = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Nike", callback_data="nike_shoes"),
+        ],
+        {
+            InlineKeyboardButton(text="Puma", callback_data="puma_shoes"),
+        },
+        {
+            InlineKeyboardButton(text="Reebok", callback_data="reebok_shoes"),
+        },
+        {
+            InlineKeyboardButton(text="Alexander Mcqueen", callback_data="alex_shoes"),
+        },
+        {
+            InlineKeyboardButton(text="Louis Vuitton", callback_data="louis_shoes"),
+        },
     ],
 )
 
@@ -61,6 +107,8 @@ async def start_cmd(message: types.Message):
 
 @dp.callback_query(F.data == "t_shirt")
 async def replik(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAP0ZfMDahGv-n4NdkTwQgky4yesQMUAAsnWMRs9CJlLTJjnvmGbsh8BAAMCAAN4AAM0BA")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAP2ZfMDdLh6nnHi0dIpDDJAAy8FG90AAsrWMRs9CJlLOYbpSK6RKH0BAAMCAAN4AAM0BA")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAP4ZfMDeuVA_S2AwIRdgYVsYWLRzyEAAsvWMRs9CJlL7tOfQ0IjdW0BAAMCAAN5AAM0BA")
@@ -75,13 +123,15 @@ async def replik(callback: types.CallbackQuery):
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBBmXzA6NJ7wFs8o7XTKsoD6J2_w0IAALW1jEbPQiZSyhmtEt30d_EAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAP-ZfMDo2JbNLVCZOQg9UiRBvbem44AAs7WMRs9CJlL4LLFxxAUQnYBAAMCAAN5AAM0BA")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBEGXzBXcEKoQlcUD19fxJNESsDZ7SAALo1jEb9TqYS_Hfs0LJK1r0AQADAgADeAADNAQ",
-                                        caption="У нас вы найдете футболки PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете футболки PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: от 1 490,00 ₽ - до 2 490,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 @dp.callback_query(F.data == "zip")
 async def zip(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBkWXzCORQUioVvvURbA70E1mzDS38AAIL1zEbPQiZS8HS0vzy9H6cAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBlGXzCOTlQseQJ1gMDjyHVZakCjOxAAIO1zEbPQiZSyDYC3JyP66LAQADAgADeAADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBjWXzCOT2rbU6TCYAAfnIRgkU5CKCVwACB9cxGz0ImUu7vNwJ4qXr2gEAAwIAA3kAAzQE")
@@ -92,13 +142,15 @@ async def zip(callback: types.CallbackQuery):
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBkmXzCOQyPuV5CYfzKHTl79HT-4KlAAIM1zEbPQiZS5jbCUELJ78iAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBj2XzCOTQkiI9eb4kNhnwVPD5zg7HAAIJ1zEbPQiZS9OKu8WXEt6tAQADAgADeAADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIBoGXzCV-_UIBdiOmPoHIV917ASqQnAAL41jEb9TqYSwZuL9LX8zPzAQADAgADeAADNAQ",
-                                        caption="У нас вы найдете зипки PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете зипки PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: от 3 490,00 ₽ - до 4 490,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 @dp.callback_query(F.data == "sweater")
 async def sweater(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICDWXzDLK5ZaPTt7w0YDMox7k8AAFIGgACJNcxGz0ImUtRx2zqHOoDRQEAAwIAA3gAAzQE")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICC2XzDLJuDjrjEYRKxjDQRf_i47DMAAIi1zEbPQiZS9znJhGyBB--AQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICEGXzDLKM0j93xGVGZ-UDtdycSks4AAIp1zEbPQiZSxWlJExyrIzGAQADAgADeQADNAQ")
@@ -108,25 +160,29 @@ async def sweater(callback: types.CallbackQuery):
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICD2XzDLKb-NU_aVOFUk36QahXayn3AAIo1zEbPQiZSxjk4OGR86cLAQADAgADeQADNAQ")
 
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICGGXzDYBXIvbg4n7fsXwyjg9waiNWAAII1zEb9TqYS4pkjyDtxQ9GAQADAgADeAADNAQ",
-                                        caption="У нас вы найдете кофты PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете кофты PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: от 3 990,00 ₽ - до 4 990,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 @dp.callback_query(F.data == "cap")
 async def cap(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICNGXzDp-6O0mNxQHOSARXFCRhvd9DAAIx1zEbPQiZSwl9x7uhNo_JAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICM2XzDp9sAAGDHwqxWDEp_D7__IP-sgACMNcxGz0ImUtumz1fVAqCZAEAAwIAA3gAAzQE")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICMmXzDp8Rr7-ZmO7ghXHNkw3zrbbYAAIv1zEbPQiZS6nM7T0AAbZuGAEAAwIAA3kAAzQE")
 
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICPGXzD3dXvIvQWVM9RHXrZmenRpmoAAIL1zEb9TqYSzBZj8Fq64l6AQADAgADeAADNAQ",
-                                        caption="У нас вы найдете головной убор/очки PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете головной убор/очки PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: от 0 490,00 ₽ - до 1 090,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 @dp.callback_query(F.data == "shoes")
 async def shoes(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICeGXzEKAqiLYbPAExQAdfqbrkYnrBAAJO1zEbPQiZS2v4NdFY7R1hAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICe2XzEKCZne_zX9Wk4eP1fsYuBJdPAAJR1zEbPQiZS4j7Z69EVNhMAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICeWXzEKAiay1GZE8PKdDQPRxshiLdAAJP1zEbPQiZS3BHrF6IqoqoAQADAgADeQADNAQ")
@@ -134,13 +190,15 @@ async def shoes(callback: types.CallbackQuery):
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICd2XzEKBMykVCiTmxC--X7fY7UCZTAAJN1zEbPQiZS6GWHpTYSB66AQADAgADeQADNAQ")
 
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICgWXzEgvg4nb1Jm1Uu3o3TAKQOgJdAAIS1zEb9TqYS8DUzXbLOSqrAQADAgADeAADNAQ",
-                                        caption="У нас вы найдете кроссовки PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете кроссовки PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: 3 490,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 @dp.callback_query(F.data == "trousers")
 async def trousers(callback: types.CallbackQuery):
+    await callback.answer("")
+
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICkmXzE0KWLqrMtpaMFc8w4jfiWaXmAAJi1zEbPQiZS-dz88g_13hoAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICkWXzE0LDaUEn2BrBEdZjmYzKoSp9AAJh1zEbPQiZS5a0-Nvl-KOFAQADAgADeQADNAQ")
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICmGXzFF8uFHZuDnf-9Ue2xtrRjcprAAJn1zEbPQiZS7ba7wXCp4GwAQADAgADeQADNAQ")
@@ -148,18 +206,29 @@ async def trousers(callback: types.CallbackQuery):
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAICl2XzFF8HZK0KjE9pu8FGSDZOk90nAAJm1zEbPQiZS6Avk0DQy_IRAQADAgADeQADNAQ")
 
     await callback.message.answer_photo(photo="AgACAgIAAxkBAAIClWXzFBNfl5_StSr4uOmAuO1b3VjgAAIX1zEb9TqYS8AK06eDO2nPAQADAgADeAADNAQ",
-                                        caption="У нас вы найдете штаны PREMIUM качества, которая ничем не отличается от оригинала\n"
+                                        caption="У нас вы найдете штаны PREMIUM качества, а также дешёвую стоимость\n"
                                               "\nЦена: от 1 990,00 ₽ - до 2 490,00 ₽\n\nЕсли у вас есть вопросы или требуется помощь,"
                                               " обратитесь к нашему менеджеру.\n\nПолный ассортимент моделей👉🏻 "
                                               "https://t.me/ff_poizon", reply_markup=supp)
 
 
+@dp.callback_query(F.data == "brend")
+async def brend(callback: types.CallbackQuery):
+    await callback.answer("Бренды/Оригинальные товары", show_alert=True)
+    await callback.message.answer_photo(photo="AgACAgIAAxkBAAIDamXzTyF39ef90ZOvn979XmEdQ2WyAAJw2DEb9TqYS77uBMcjD9OyAQADAgADeAADNAQ",
+                                        reply_markup=brand)
 
 
+@dp.callback_query(F.data == "shoes_brand")
+async def shoes_brand(callback: types.CallbackQuery):
+    await callback.answer("Shoes")
+
+    await callback.message.edit_caption(caption="<em>Выберите бренд:</em>", parse_mode="HTML", reply_markup=brand_shoes)
 
 
 @dp.callback_query(F.data == "back")
 async def back(callback: types.CallbackQuery):
+    await callback.answer("")
     await callback.message.answer(
         f"<em>Здравствуйте, <b>{callback.message.from_user.full_name}</b>! Я бот магазина FF Poizon. Я могу прислать подробные фото "
         f"о каждом товаре. Для этого выберите кнопкой ниже нужную категорию "
